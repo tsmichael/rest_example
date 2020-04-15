7dc41463e2fdf26d55b610317f92cc985e2012fe
Mykhailo Tsalan <43215116+tsmichael@users.noreply.github.com>
2020-04-15 10:18:07 +0300

    Update ConstantVariables.java

--- Rest_Demo/src/com/petstore/data/ConstantVariables.java
+++ Rest_Demo/src/com/petstore/data/ConstantVariables.java
@@ -2,6 +2,6 @@ package com.petstore.data;
 
 public final class ConstantVariables {
     public static final String API_URL = System.getenv("API_URL");
-    public static final Integer PORT = System.getenv("API_PORT");
+    public static final Integer API_PORT = System.getenv("API_PORT");
 
 }
